b4fccc078496758c182e03e68d5b37e1f44b5b53
prab-pip <prab.interview.prep@gmail.com>
2016-07-03 17:54:15 -0400

    My solution in earlier commit had multiple pain points. I was trying to do O(x) operation where x is the greater of m and n. But, I had to take care of so many cases and still the solution had inherent flaws. So I had to abandon it. It was a very nice learning.
    This is the solution mentioned in the CTCI book by Gayle :-
    
    1. Left-fill the smaller list with zeroes. O(m) + O(n) operation for finding the smaller list. Then O(abs(m-n)) for left-fill on the smaller list
    2. Then,  O(n) (or O(m), because after step 1, both n and m will be same) for the sum operation + O(1) to take care of the left-over carry operation, in case it is non-zero
    
    Written the code and added lots of tests to make sure the solution is good and does not fail where my earlier code failed.

--- CrackingTheCodingInterview_5ed/ChapterTwo_LinkedLists/TestsForCrackingTheCodingInterviewQuestions/Test_TwoDotFive_AddTwoNonReversedIntegersAsLinkedLists.py
+++ CrackingTheCodingInterview_5ed/ChapterTwo_LinkedLists/TestsForCrackingTheCodingInterviewQuestions/Test_TwoDotFive_AddTwoNonReversedIntegersAsLinkedLists.py
@@ -8,11 +8,11 @@ class Test_TwoDotFive_AddTwoNonReversedIntegersAsLinkedLists_Without_LeftOver_Ca
     def test_add_without_carry_basic_case_both_lists_empty(self):
         firstList = SinglyLinkedList()
         secondList = SinglyLinkedList()
-
-        s = Solution()
         outputList = SinglyLinkedList()
 
-        s.addTwoNonReversedIntegersAsLinkedListsAndCreateNewList(firstList.head, secondList.head, outputList)
+        s = Solution(firstList, secondList, outputList)
+        s.addTwoNonReversedIntegersAsLinkedListsAndCreateNewList()
+
         outputValue = outputList.returnLinkedListAsList()
         expectedValue = []
 
@@ -21,13 +21,13 @@ class Test_TwoDotFive_AddTwoNonReversedIntegersAsLinkedLists_Without_LeftOver_Ca
     def test_add_without_carry_basic_case_first_list_empty_second_list_has_one_element(self):
         firstList = SinglyLinkedList()
         secondList = SinglyLinkedList()
+        outputList = SinglyLinkedList()
 
         inputValue = [1]
         secondList.populate(inputValue)
+        s = Solution(firstList, secondList, outputList)
 
-        s = Solution()
-        outputList = SinglyLinkedList()
-        s.addTwoNonReversedIntegersAsLinkedListsAndCreateNewList(firstList.head, secondList.head, outputList)
+        s.addTwoNonReversedIntegersAsLinkedListsAndCreateNewList()
         outputValue = outputList.returnLinkedListAsList()
         expectedValue = inputValue
 
@@ -35,63 +35,63 @@ class Test_TwoDotFive_AddTwoNonReversedIntegersAsLinkedLists_Without_LeftOver_Ca
 
     def test_add_without_carry_basic_case_second_list_empty_first_list_has_one_element(self):
         firstList = SinglyLinkedList()
+        secondList = SinglyLinkedList()
+        outputList = SinglyLinkedList()
+
         inputValue = [1]
         firstList.populate(inputValue)
 
-        secondList = SinglyLinkedList()
+        s = Solution(firstList, secondList, outputList)
+        s.addTwoNonReversedIntegersAsLinkedListsAndCreateNewList()
 
-        s = Solution()
-        outputList = SinglyLinkedList()
-        s.addTwoNonReversedIntegersAsLinkedListsAndCreateNewList(firstList.head, secondList.head, outputList)
         outputValue = outputList.returnLinkedListAsList()
         expectedValue = inputValue
 
         self.assertTrue(outputValue == expectedValue, "Expected: %s, Got: %s" %(expectedValue, outputValue))
 
     def test_without_carry_best_case_equal_elements_one_in_length(self):
-                firstList = SinglyLinkedList()
-                firstList.populate([1])
+        firstList = SinglyLinkedList()
+        secondList = SinglyLinkedList()
+        outputList = SinglyLinkedList()
 
-                secondList = SinglyLinkedList()
-                secondList.populate([2])
+        firstList.populate([1])
+        secondList.populate([2])
 
-                outputList = SinglyLinkedList()
-                s = Solution()
-                s.addTwoNonReversedIntegersAsLinkedListsAndCreateNewList(firstList.head, secondList.head, outputList)
+        s = Solution(firstList, secondList, outputList)
+        s.addTwoNonReversedIntegersAsLinkedListsAndCreateNewList()
 
-                outputValue = outputList.returnLinkedListAsList()
+        outputValue = outputList.returnLinkedListAsList()
 
-                expectedValue = [3]
+        expectedValue = [3]
 
-                self.assertTrue(outputValue == expectedValue, "Expected: %s, Got: %s" %(expectedValue, outputValue))
+        self.assertTrue(outputValue == expectedValue, "Expected: %s, Got: %s" %(expectedValue, outputValue))
 
     def test_without_carry_best_case_equal_elements_two_in_length(self):
         firstList = SinglyLinkedList()
-        firstList.populate([1, 1])
-
         secondList = SinglyLinkedList()
+        outputList = SinglyLinkedList()
+
+        firstList.populate([1, 1])
         secondList.populate([2, 3])
 
-        outputList = SinglyLinkedList()
-        s = Solution()
-        s.addTwoNonReversedIntegersAsLinkedListsAndCreateNewList(firstList.head, secondList.head, outputList)
+        s = Solution(firstList, secondList, outputList)
+        s.addTwoNonReversedIntegersAsLinkedListsAndCreateNewList()
 
         outputValue = outputList.returnLinkedListAsList()
 
         expectedValue = [3, 4]
         self.assertTrue(outputValue == expectedValue, "Expected: %s, Got: %s" %(expectedValue, outputValue))
 
-
     def test_without_carry_best_case_equal_elements_three_in_length(self):
         firstList = SinglyLinkedList()
-        firstList.populate([1, 1, 8])
-
         secondList = SinglyLinkedList()
+        outputList = SinglyLinkedList()
+
+        firstList.populate([1, 1, 8])
         secondList.populate([4, 3, 1])
 
-        outputList = SinglyLinkedList()
-        s = Solution()
-        s.addTwoNonReversedIntegersAsLinkedListsAndCreateNewList(firstList.head, secondList.head, outputList)
+        s = Solution(firstList, secondList, outputList)
+        s.addTwoNonReversedIntegersAsLinkedListsAndCreateNewList()
 
         outputValue = outputList.returnLinkedListAsList()
 
@@ -100,15 +100,14 @@ class Test_TwoDotFive_AddTwoNonReversedIntegersAsLinkedLists_Without_LeftOver_Ca
 
     def test_add_without_carry_basic_case_second_list_empty_first_list_has_more_than_one_element(self):
         firstList = SinglyLinkedList()
+        secondList = SinglyLinkedList()
+        outputList = SinglyLinkedList()
+
         inputValue = range(10)
         firstList.populate(inputValue)
 
-        secondList = SinglyLinkedList()
-
-        outputList = SinglyLinkedList()
-
-        s = Solution()
-        s.addTwoNonReversedIntegersAsLinkedListsAndCreateNewList(firstList.head, secondList.head, outputList)
+        s = Solution(firstList, secondList, outputList)
+        s.addTwoNonReversedIntegersAsLinkedListsAndCreateNewList()
 
         outputValue = outputList.returnLinkedListAsList()
         expectedValue = inputValue
@@ -118,101 +117,95 @@ class Test_TwoDotFive_AddTwoNonReversedIntegersAsLinkedLists_Without_LeftOver_Ca
 
     def test_add_without_carry_basic_case_first_list_empty_second_list_has_more_than_one_element(self):
         firstList = SinglyLinkedList()
-
         secondList = SinglyLinkedList()
+        outputList = SinglyLinkedList()
+
         inputValue = range(10)
         secondList.populate(inputValue)
 
-        outputList = SinglyLinkedList()
-
-        s = Solution()
-        s.addTwoNonReversedIntegersAsLinkedListsAndCreateNewList(firstList.head, secondList.head, outputList)
+        s = Solution(firstList, secondList, outputList)
+        s.addTwoNonReversedIntegersAsLinkedListsAndCreateNewList()
 
         outputValue = outputList.returnLinkedListAsList()
         expectedValue = inputValue
 
         self.assertTrue(outputValue == expectedValue, "Expected: %s, Got: %s" %(expectedValue, outputValue))
-#
-#     def test_add_without_carry_both_lists_have_equal_items(self):
-#         firstList = SinglyLinkedList()
-#         firstList.populate([7,1,6])
-#
-#         secondList = SinglyLinkedList()
-#         secondList.populate([5,9,2])
-#
-#
-#         s = Solution(firstList, secondList)
-#
-#         returnLinkedList = s.addTwoReversedIntegersAsLinkedListsAndCreateNewList()
-#         outputValue = returnLinkedList.returnLinkedListAsList()
-#         expectedValue = [2,1,9]
-#         self.assertTrue(outputValue == expectedValue, "Expected: %s, Got: %s" %(expectedValue, outputValue))
-#
+
+    def test_add_without_carry_both_lists_have_equal_items(self):
+        firstList = SinglyLinkedList()
+        secondList = SinglyLinkedList()
+        outputList = SinglyLinkedList()
+
+        firstList.populate([7,1,6])
+        secondList.populate([1,9,2])
+
+        s = Solution(firstList, secondList, outputList)
+
+        s.addTwoNonReversedIntegersAsLinkedListsAndCreateNewList()
+        outputValue = outputList.returnLinkedListAsList()
+        expectedValue = [9,0,8]
+        self.assertTrue(outputValue == expectedValue, "Expected: %s, Got: %s" %(expectedValue, outputValue))
+
 #
     def test_add_without_left_over_carry_both_lists_have_unequal_items(self):
         firstList = SinglyLinkedList()
-        firstList.populate([7,1,6,9])
-
         secondList = SinglyLinkedList()
+        outputList = SinglyLinkedList()
+
+        firstList.populate([7,1,6,9])
         secondList.populate([5,9,2])
 
-        outputList = SinglyLinkedList()
-        s = Solution()
-        s.addTwoNonReversedIntegersAsLinkedListsAndCreateNewList(firstList.head, secondList.head, outputList)
+        s = Solution(firstList, secondList, outputList)
+        s.addTwoNonReversedIntegersAsLinkedListsAndCreateNewList()
+
         outputValue = outputList.returnLinkedListAsList()
         expectedValue = [7,7,6,1]
-        print "Expected: %s, Got: %s" %(expectedValue, outputValue)
         self.assertTrue(outputValue == expectedValue, "Expected: %s, Got: %s" %(expectedValue, outputValue))
-#
-#
-#
-#
+
 class Test_TwoDotFive_AddTwoReversedIntegersAsLinkedLists_With_LeftOver_Carry_Tests(unittest.TestCase):
     def test_with_carry_best_case_equal_elements_one_in_length(self):
         firstList = SinglyLinkedList()
-        firstList.populate([8])
-
         secondList = SinglyLinkedList()
+        outputList = SinglyLinkedList()
+
+        firstList.populate([8])
         secondList.populate([2])
 
-        outputList = SinglyLinkedList()
-        s = Solution()
-        s.addTwoNonReversedIntegersAsLinkedListsAndCreateNewList(firstList.head, secondList.head, outputList)
+        s = Solution(firstList, secondList, outputList)
+        s.addTwoNonReversedIntegersAsLinkedListsAndCreateNewList()
 
         outputValue = outputList.returnLinkedListAsList()
 
         expectedValue = [1, 0]
-
         self.assertTrue(outputValue == expectedValue, "Expected: %s, Got: %s" %(expectedValue, outputValue))
 
     def test_with_carry_best_case_equal_elements_two_in_length(self):
         firstList = SinglyLinkedList()
-        firstList.populate([7, 1])
-
         secondList = SinglyLinkedList()
+        outputList = SinglyLinkedList()
+
+        firstList.populate([7, 1])
         secondList.populate([6, 9])
 
-        outputList = SinglyLinkedList()
-        s = Solution()
-        s.addTwoNonReversedIntegersAsLinkedListsAndCreateNewList(firstList.head, secondList.head, outputList)
+        s = Solution(firstList, secondList, outputList)
+        s.addTwoNonReversedIntegersAsLinkedListsAndCreateNewList()
 
         outputValue = outputList.returnLinkedListAsList()
 
         expectedValue = [1, 4, 0]
-
         self.assertTrue(outputValue == expectedValue, "Expected: %s, Got: %s" %(expectedValue, outputValue))
 
 
     def test_with_carry_best_case_equal_elements_three_in_length(self):
         firstList = SinglyLinkedList()
-        firstList.populate([6, 4, 9])
-
         secondList = SinglyLinkedList()
+        outputList = SinglyLinkedList()
+
+        firstList.populate([6, 4, 9])
         secondList.populate([7, 7, 9])
 
-        outputList = SinglyLinkedList()
-        s = Solution()
-        s.addTwoNonReversedIntegersAsLinkedListsAndCreateNewList(firstList.head, secondList.head, outputList)
+        s = Solution(firstList, secondList, outputList)
+        s.addTwoNonReversedIntegersAsLinkedListsAndCreateNewList()
 
         outputValue = outputList.returnLinkedListAsList()
 
@@ -220,32 +213,18 @@ class Test_TwoDotFive_AddTwoReversedIntegersAsLinkedLists_With_LeftOver_Carry_Te
         self.assertTrue(outputValue == expectedValue, "Expected: %s, Got: %s" %(expectedValue, outputValue))
 
 
-    #     def test_add_with_carry_both_lists_have_one_item(self):
-#         firstList = SinglyLinkedList()
-#         firstList.populate([9])
-#
-#         secondList = SinglyLinkedList()
-#         secondList.populate([1])
-#
-#
-#         s = Solution(firstList, secondList)
-#
-#         returnLinkedList = s.addTwoReversedIntegersAsLinkedListsAndCreateNewList()
-#         outputValue = returnLinkedList.returnLinkedListAsList()
-#         expectedValue = [0,1]
-#         self.assertTrue(outputValue == expectedValue, "Expected: %s, Got: %s" %(expectedValue, outputValue))
-#
-#     def test_add_with_carry_forever_carry_till_last(self):
-#         firstList = SinglyLinkedList()
-#         firstList.populate([9,9,9,9,9,9,9])
-#
-#         secondList = SinglyLinkedList()
-#         secondList.populate([1])
-#
-#
-#         s = Solution(firstList, secondList)
-#
-#         returnLinkedList = s.addTwoReversedIntegersAsLinkedListsAndCreateNewList()
-#         outputValue = returnLinkedList.returnLinkedListAsList()
-#         expectedValue = [0,0,0,0,0,0,0,1]
-#         self.assertTrue(outputValue == expectedValue, "Expected: %s, Got: %s" %(expectedValue, outputValue))
+
+    def test_add_with_carry_forever_carry_till_last(self):
+        firstList = SinglyLinkedList()
+        secondList = SinglyLinkedList()
+        outputList = SinglyLinkedList()
+
+        firstList.populate([9,9,9,9,9,9,9])
+        secondList.populate([1])
+
+        s = Solution(firstList, secondList, outputList)
+        s.addTwoNonReversedIntegersAsLinkedListsAndCreateNewList()
+
+        outputValue = outputList.returnLinkedListAsList()
+        expectedValue = [1, 0, 0, 0, 0, 0, 0, 0]
+        self.assertTrue(outputValue == expectedValue, "Expected: %s, Got: %s" %(expectedValue, outputValue))
--- CrackingTheCodingInterview_5ed/ChapterTwo_LinkedLists/TwoDotFive_AddTwoNonReversedIntegersAsLinkedLists.py
+++ CrackingTheCodingInterview_5ed/ChapterTwo_LinkedLists/TwoDotFive_AddTwoNonReversedIntegersAsLinkedLists.py
@@ -1,49 +1,44 @@
 class Solution(object):
     """
     This solution assumes that the data in each linked list node is integer.
+
+    # Important step for this question:-
+    1. Because 2 lists could differ in length. left-fill the smaller list with zeroes
     """
-    def __init__(self):
-        pass
+    def __init__(self, list1, list2, outputList):
+        self.list1 = list1
+        self.list2 = list2
+        self.outputList = outputList
 
-    def addTwoNonReversedIntegersAsLinkedListsAndCreateNewList(self, list1CurrentNode, list2CurrentNode, outputList, firstListCount=0, secondListCount=0):
+    def insertZeroesAtHead(self, linkedListObject, numZeroes):
+        for zero in xrange(numZeroes):
+            linkedListObject.insertAtHead(0)
 
-        if list1CurrentNode is None and list2CurrentNode is None:
-            return (firstListCount, secondListCount, 0)
-        elif list1CurrentNode is None and list2CurrentNode is not None:
-            numOperationsAllowedOnList1, numOperationsAllowedOnList2, carryFromPreviousAdd = self.addTwoNonReversedIntegersAsLinkedListsAndCreateNewList(list1CurrentNode, list2CurrentNode.nextPointer, outputList, firstListCount, secondListCount+1)
-        elif list1CurrentNode is not None and list2CurrentNode is None:
-            numOperationsAllowedOnList1, numOperationsAllowedOnList2, carryFromPreviousAdd = self.addTwoNonReversedIntegersAsLinkedListsAndCreateNewList(list1CurrentNode.nextPointer, list2CurrentNode, outputList, firstListCount+1, secondListCount)
-        else:  # Both lists' current nodes are not None. Best Case
-            numOperationsAllowedOnList1, numOperationsAllowedOnList2, carryFromPreviousAdd = self.addTwoNonReversedIntegersAsLinkedListsAndCreateNewList(list1CurrentNode.nextPointer, list2CurrentNode.nextPointer, outputList, firstListCount+1, secondListCount+1)
-
-        # print 'here??', numOperationsAllowedOnList1, numOperationsAllowedOnList2
-
-        if numOperationsAllowedOnList1 != 0 and numOperationsAllowedOnList2 != 0:
-            # print "list1CurrentNode.data: %s, list2CurrentNode.data: %s" % (list1CurrentNode.data, list2CurrentNode.data )
-            sum = list1CurrentNode.data + list2CurrentNode.data + carryFromPreviousAdd
-        elif numOperationsAllowedOnList1 == 0:  # Just check whats on second list
-            sum = list2CurrentNode.data + carryFromPreviousAdd
-        elif numOperationsAllowedOnList2 == 0:  # Just check whats on first list
-            sum = list1CurrentNode.data + carryFromPreviousAdd
-
-        data = sum%10
-        carry = sum/10
-        outputList.insertAtHead(data)
-
-        if carry != 0 and (numOperationsAllowedOnList1 - 1 == 0 or numOperationsAllowedOnList2 - 1 == 0):
-            # Take care of the left-over carry from the last step
-            outputList.insertAtHead(carry)
-        if numOperationsAllowedOnList1 == 0 and numOperationsAllowedOnList2 == 0:
-            return numOperationsAllowedOnList1, numOperationsAllowedOnList2, carry
-        elif numOperationsAllowedOnList1 != 0 and numOperationsAllowedOnList2 == 0:
-            return numOperationsAllowedOnList1 - 1, numOperationsAllowedOnList2, carry
-        elif numOperationsAllowedOnList1 == 0 and numOperationsAllowedOnList2 != 0:
-            return numOperationsAllowedOnList1, numOperationsAllowedOnList2-1, carry
+    def add_now(self, list1CurrentNode, list2CurrentNode):
+        if list1CurrentNode is None:
+            return 0  # Returning 0 as carry for first use
         else:
-            return numOperationsAllowedOnList1-1, numOperationsAllowedOnList2-1, carry
+            carryReturned = self.add_now(list1CurrentNode.nextPointer, list2CurrentNode.nextPointer)
+
+            sum = carryReturned + list1CurrentNode.data + list2CurrentNode.data
+
+            data = sum % 10
+            self.outputList.insertAtHead(data)
+
+            carryGenerated = sum / 10
+            return carryGenerated
 
 
+    def addTwoNonReversedIntegersAsLinkedListsAndCreateNewList(self):
+        lenList1 = len(self.list1)
+        lenList2 = len(self.list2)
 
+        diff = lenList1 - lenList2
+        if diff < 0:  # Means list1 is smaller than list2
+            self.insertZeroesAtHead(self.list1, -diff)
+        else:
+            self.insertZeroesAtHead(self.list2, diff)
 
-
-
+        carryReturned = self.add_now(self.list1.head, self.list2.head)
+        if carryReturned != 0:
+            self.outputList.insertAtHead(carryReturned)
