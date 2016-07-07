df2da5e0dd08b0fd92d79d3f8bb577a2d15b2946
prab-pip <prab.interview.prep@gmail.com>
2016-07-06 21:27:43 -0400

    Implemented Gayle's solution of finding whether Linked List is a palindrome or not. Pointed the earlier written tests to this method2 and made sure they pass

--- CrackingTheCodingInterview_5ed/ChapterTwo_LinkedLists/TestsForCrackingTheCodingInterviewQuestions/Test_TwoDotSeven_CheckIfSinglyLinkedListisAPalindrome.py
+++ CrackingTheCodingInterview_5ed/ChapterTwo_LinkedLists/TestsForCrackingTheCodingInterviewQuestions/Test_TwoDotSeven_CheckIfSinglyLinkedListisAPalindrome.py
@@ -99,3 +99,126 @@ class Test_CheckIfSinglyLinkedListisAPalindrome_method1(unittest.TestCase):
         expectedValue = True  # Considering empty list is a palindrome
         receivedValue = s.method1()
         self.assertTrue(expectedValue == receivedValue, "Expected: %s, Received: %s" %(expectedValue, receivedValue))
+
+
+class Test_CheckIfSinglyLinkedListisAPalindrome_method2(unittest.TestCase):
+    def test_empty_list(self):
+        singlyLinkedList = SinglyLinkedList()
+
+        s = Solution(singlyLinkedList)
+
+        expectedValue = True  # Considering empty list is a palindrome
+        receivedValue = s.method2()
+        self.assertTrue(expectedValue == receivedValue, "Expected: %s, Received: %s" %(expectedValue, receivedValue))
+
+    def test_list_with_one_value(self):
+        singlyLinkedList = SinglyLinkedList()
+
+        inputList = [1]
+        singlyLinkedList.populate(inputList)
+
+        s = Solution(singlyLinkedList)
+
+        expectedValue = True  # Considering empty list is a palindrome
+        receivedValue = s.method2()
+        self.assertTrue(expectedValue == receivedValue, "Expected: %s, Received: %s" %(expectedValue, receivedValue))
+
+    def test_list_with_two_values_not_a_palindrome(self):
+        singlyLinkedList = SinglyLinkedList()
+
+        inputList = [1, 2]
+        singlyLinkedList.populate(inputList)
+
+        s = Solution(singlyLinkedList)
+
+        expectedValue = False  # Considering empty list is a palindrome
+        receivedValue = s.method2()
+        self.assertTrue(expectedValue == receivedValue, "Expected: %s, Received: %s" %(expectedValue, receivedValue))
+
+
+    def test_list_with_two_values_is_a_palindrome(self):
+        singlyLinkedList = SinglyLinkedList()
+
+        inputList = [1, 1]
+        singlyLinkedList.populate(inputList)
+
+        s = Solution(singlyLinkedList)
+
+        expectedValue = True  # Considering empty list is a palindrome
+        receivedValue = s.method1()
+        self.assertTrue(expectedValue == receivedValue, "Expected: %s, Received: %s" %(expectedValue, receivedValue))
+
+    def test_list_with_three_values_not_a_palindrome(self):
+        singlyLinkedList = SinglyLinkedList()
+
+        inputList = [1, 2, 3]
+        singlyLinkedList.populate(inputList)
+
+        s = Solution(singlyLinkedList)
+
+        expectedValue = False  # Considering empty list is a palindrome
+        receivedValue = s.method2()
+        self.assertTrue(expectedValue == receivedValue, "Expected: %s, Received: %s" %(expectedValue, receivedValue))
+
+
+    def test_list_with_three_values_is_a_palindrome(self):
+        singlyLinkedList = SinglyLinkedList()
+
+        inputList = [1, 2, 1]
+        singlyLinkedList.populate(inputList)
+
+        s = Solution(singlyLinkedList)
+
+        expectedValue = True  # Considering empty list is a palindrome
+        receivedValue = s.method2()
+        self.assertTrue(expectedValue == receivedValue, "Expected: %s, Received: %s" %(expectedValue, receivedValue))
+
+    def test_list_with_four_values_not_a_palindrome(self):
+        singlyLinkedList = SinglyLinkedList()
+
+        inputList = [1, 2, 1, 2]
+        singlyLinkedList.populate(inputList)
+
+        s = Solution(singlyLinkedList)
+
+        expectedValue = False  # Considering empty list is a palindrome
+        receivedValue = s.method2()
+        self.assertTrue(expectedValue == receivedValue, "Expected: %s, Received: %s" %(expectedValue, receivedValue))
+
+
+    def test_list_with_four_values_is_a_palindrome(self):
+        singlyLinkedList = SinglyLinkedList()
+
+        inputList = [1, 2, 2, 1]
+        singlyLinkedList.populate(inputList)
+
+        s = Solution(singlyLinkedList)
+
+        expectedValue = True  # Considering empty list is a palindrome
+        receivedValue = s.method2()
+        self.assertTrue(expectedValue == receivedValue, "Expected: %s, Received: %s" %(expectedValue, receivedValue))
+
+    def test_list_with_five_values_not_a_palindrome(self):
+        singlyLinkedList = SinglyLinkedList()
+
+        inputList = [1, 2, 3, 2, 4]
+        singlyLinkedList.populate(inputList)
+
+        s = Solution(singlyLinkedList)
+
+        expectedValue = False  # Considering empty list is a palindrome
+        receivedValue = s.method2()
+        self.assertTrue(expectedValue == receivedValue, "Expected: %s, Received: %s" %(expectedValue, receivedValue))
+
+
+    def test_list_with_five_values_is_a_palindrome(self):
+        singlyLinkedList = SinglyLinkedList()
+
+        inputList = [1, 2, 3, 2, 1]
+        singlyLinkedList.populate(inputList)
+
+        s = Solution(singlyLinkedList)
+
+        expectedValue = True  # Considering empty list is a palindrome
+        receivedValue = s.method2()
+        self.assertTrue(expectedValue == receivedValue, "Expected: %s, Received: %s" %(expectedValue, receivedValue))
--- CrackingTheCodingInterview_5ed/ChapterTwo_LinkedLists/TwoDotSeven_CheckIfSinglyLinkedListisAPalindrome.py
+++ CrackingTheCodingInterview_5ed/ChapterTwo_LinkedLists/TwoDotSeven_CheckIfSinglyLinkedListisAPalindrome.py
@@ -32,3 +32,41 @@ class Solution(object):
 
         # print "Linked List is a palindrome"
         return True
+
+    def _palindromeOrNot(self, leftSideNode, length):
+        if length == 0:
+            return leftSideNode
+        elif length == 1:
+            return leftSideNode.nextPointer
+        else:
+            rightSideNode = self._palindromeOrNot(leftSideNode.nextPointer, length - 2)
+            if rightSideNode is False:
+                return False
+            elif leftSideNode.data != rightSideNode.data:
+                # print "not a palindrome. %s != %s" % (leftSideNode.data, rightSideNode.data)
+                return False
+            else:
+                return rightSideNode.nextPointer
+
+
+    def method2(self):
+        '''
+        Implementing the solution as mentioned in the CTCI book:-
+        1. Get the length of the list
+        2. use the value of length to judge whether we've reached the centre of the list
+        3. In the base case, return the next node of the middle
+        4. In the recursive case, compare the current node data vs returned node data
+        '''
+        length = len(self.singlyLinkedListObject)
+        if length == 0:
+            return True  # Considering a Linked List with zero length is a palindrome
+        elif length == 1:
+            return True  # A Linked List with zero length is a palindrome
+        else:
+            returnedStatus = self._palindromeOrNot(self.singlyLinkedListObject.head, length)
+            if returnedStatus is False:
+                return False
+            elif returnedStatus is None:
+                return True
+            # Short form of above code: return returnedStatus is None
+
